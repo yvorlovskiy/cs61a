27dc355aa9943abc7656853c1bb12c1c53b24e75
Yury Orlovskiy <yuryorlovskiy@Yurys-iMac.local>
2022-03-02 12:47:35 -0800

    hw 4 added

---
+++ hws/hw04/parsons_probs/smartfridge.py
@@ -0,0 +1,26 @@
+class SmartFridge:
+    """"
+    >>> fridgey = SmartFridge()
+    >>> fridgey.add_item('Mayo', 1)
+    'I now have 1 Mayo'
+    >>> fridgey.add_item('Mayo', 2)
+    'I now have 3 Mayo'
+    >>> fridgey.use_item('Mayo', 2.5)
+    'I have 0.5 Mayo left'
+    >>> fridgey.use_item('Mayo', 0.5)
+    'Uh oh, buy more Mayo!'
+    """
+    def __init__(self):
+        self.items = {}
+    def add_item(self, item, quantity):
+        self.items[item] += quantity
+        return f'I now have {self.items[item]} {item}'
+    def use_item(self, item, quantity):
+        self.quantity = quantity
+        if item in self.items:
+            self.items[item] -= min(self.quantity, self.items[item] - self.quantity)
+            return f'I have {self.items[item]} {item} left'
+        if self.items[item] == 0:
+            return f'Uh oh, buy more {item}!'
+
+
